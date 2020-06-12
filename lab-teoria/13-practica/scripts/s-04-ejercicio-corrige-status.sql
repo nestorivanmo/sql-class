edc297fc114a95a2eed9dadda7907caabce5e9c8
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-12 12:01:19 -0500

    practica 13 completada

--- lab-teoria/13-practica/scripts/s-04-ejercicio-corrige-status.sql
+++ lab-teoria/13-practica/scripts/s-04-ejercicio-corrige-status.sql
@@ -3,28 +3,68 @@
 ---@Descripción:				Ejercicio 1 Práctica 13
 set serveroutput on
 
-create or replace procedure p_corrige_status (
-	p_num_expirado out number,
-	p_num_con_multa out number,
-	p_num_en_curso out number
-) is
-v_detalle_prestamo_id detalle_prestamo.detalle_prestamo_id%type;
-v_num_dias detalle_prestamo.num_dias%type;
-v_fecha_vencimiento detalle_prestamo.fecha_vencimiento%type;
-v_multa_por_pagar detalle_prestamo.multa_por_pagar%type;
-v_prestamo_id detalle_prestamo.prestamo_id%type;
-v_libro_id detalle_prestamo.libro_id%type;
-cursor cur_prestamo is
-	select * from prestamo;
+create or replace procedure p_corrige_status (p_num_expirados out number, 
+	p_num_multados out number, p_num_curso out number) is
+	cursor cur_prestamos is
+		select prestamo_id, fecha_status, status_prestamo_id
+		from prestamo;
+	v_count number;
 begin
-	for r in cur_prestamo loop
-		select detalle_prestamo_id, num_dias, fecha_vencimiento, multa_por_pagar,
-			prestamo_id, libro_id
-		into v_detalle_prestamo_id, v_num_dias, v_fecha_vencimiento, v_multa_por_pagar
-			v_prestamo_id, v_libro_id
-		from detalle_prestamo
+	p_num_expirados := 0;
+	p_num_multados 	:= 0;
+	p_num_curso 		:= 0;
+
+	for r in cur_prestamos loop
+		insert into t_detalle_prestamo
+			select detalle_prestamo_id, num_dias, fecha_vencimiento, multa_por_pagar,
+				prestamo_id, libro_id
+			from detalle_prestamo
+			where prestamo_id = r.prestamo_id;
+
+		select count(*)
+		into v_count
+		from t_detalle_prestamo
+		where multa_por_pagar > 0;
+
+		if v_count > 0 then
+			update prestamo set status_prestamo_id = 5, fecha_status = sysdate
+			where prestamo_id = r.prestamo_id;
+			p_num_multados := p_num_multados + 1;
+		else
+			select count(*)
+			into v_count
+			from t_detalle_prestamo
+			where fecha_vencimiento < sysdate;
+
+			if v_count > 0 then
+				update prestamo set status_prestamo_id = 4, fecha_status = sysdate
+				where prestamo_id = r.prestamo_id;
+				p_num_expirados := p_num_expirados + 1;
+			else
+				update prestamo set status_prestamo_id = 2, fecha_status = sysdate
+				where prestamo_id = r.prestamo_id;
+				p_num_curso := p_num_curso + 1;
+			end if;
+		end if;
+		delete from t_detalle_prestamo
 		where prestamo_id = r.prestamo_id;
 	end loop;
 end;
 /
-show errors
+show errors;
+
+declare
+	v_num_multados 	number;
+	v_num_expirados number;
+	v_num_en_curso 	number;
+begin
+	p_corrige_status(
+		p_num_expirados => v_num_expirados, 
+		p_num_multados	=> v_num_multados, 
+		p_num_curso			=> v_num_en_curso);
+	dbms_output.put_line('============== Resultados : ==============');
+	dbms_output.put_line('Cambios a Expirados: ' 	|| v_num_expirados);
+	dbms_output.put_line('Cambios a Multados: ' 	|| v_num_multados);
+	dbms_output.put_line('Cambios a En curso: ' 	|| v_num_en_curso);
+end;
+/
--- lab-teoria/13-practica/scripts/s-05-ejercicio-consulta-bibliotecas.sql
+++ lab-teoria/13-practica/scripts/s-05-ejercicio-consulta-bibliotecas.sql
@@ -3,33 +3,61 @@
 ---@Descripción:				Ejercicio 2 práctica 13
 set serveroutput on
 
-create or replace procedure p_consulta_bibliotecas(
-	p_clave_biblio in varchar2
-) is
-v_num_libros number;
-cursor cur_libros is
-	select b.biblioteca_id, b.clave, b.nombre, l.libro_id, l.isbn, l.titulo, 
-		l.tipo, lc.precio_unitario, ld.nombre_donante, 
-		(
-			count(al.asignacion_libro_id)
+create or replace procedure p_consulta_bibliotecas(p_clave_biblio varchar2) is
+	cursor cur_libros is
+		select b.biblioteca_id, b.clave, b.nombre, l.libro_id, l.isbn, l.titulo,
+			q1.total_asignados, l.tipo, lc.precio_unitario, ld.nombre_donante,
+			q2.total_prestamos
+		from biblioteca b
+		join asignacion_libro al on al.biblioteca_id = b.biblioteca_id
+		join libro l on l.libro_id = al.libro_id
+		left join libro_comprado lc on lc.libro_id = l.libro_id
+		left join libro_donado ld on ld.libro_id = l.libro_id
+		join (
+			select biblioteca_id, sum(num_copias) as total_asignados
 			from asignacion_libro al
-			where al.biblioteca_id = b.biblioteca_id
-			and al.libro_id = l.libro_id;
-		) as num_libros
-	from biblioteca b
-	join asignacion_libro al on al.biblioteca_id = b.biblioteca_id
-	join libro l on l.libro_id = al.libro_id
-	left join libro_comprado lc on lc.libro_id = l.libro_id
-	left join libro_donado ld on ld.libro_id = l.libro_id
-	group by b.biblioteca_id, b.clave, b.nombre, l.libro_id, l.isbn, l.titulo,
-		l.tipo, lc.precio_unitario, ld.nombre_donante
-	having b.clave = p_clave_biblio;
+			group by biblioteca_id
+		) q1 
+		on q1.biblioteca_id = b.biblioteca_id
+		join (
+			select libro_id, count(*) as total_prestamos
+			from detalle_prestamo
+			group by libro_id
+		) q2
+		on q2.libro_id = l.libro_id
+		where b.clave = p_clave_biblio
+		order by l.titulo;
 begin
-	v_num_libros := 0;
 	for r in cur_libros loop
-		v_num_libros := v_num_libros + 1;
+		insert into t_reporte_biblioteca(biblioteca_id, clave, nombre, 
+			total_libros_asignados, libro_id, isbn, titulo, tipo, precio_unitario, 
+			nombre_donante, total_prestamos)
+		values (r.biblioteca_id, r.clave, r.nombre, r.total_asignados, 
+			r.libro_id, r.isbn, r.titulo, r.tipo, r.precio_unitario, r.nombre_donante,
+			r.total_prestamos);
 	end loop;
-	dbms_output.put_line(v_num_libros);
 end;
 /
 show errors;
+
+declare
+	v_count number;
+begin
+	for r in (
+		select clave
+		from biblioteca
+		sample(20)
+		where rownum <= 5
+	) loop
+		p_consulta_bibliotecas(r.clave);
+		select count(*)
+		into v_count
+		from t_reporte_biblioteca;
+		dbms_output.put_line('Registros exportados para biblioteca: '
+			|| r.clave 
+			|| '  -  ' 
+			|| v_count);
+		delete from t_reporte_biblioteca;
+	end loop;		
+end;
+/
